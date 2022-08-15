eff4e7f83b495ec779955f6c2c79249c15f44bb3
Hannes Kohlberg <megaterion03@gmail.com>
2022-08-15 14:19:47 +0200

    drawin map (rocks and player) inside the window

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pygame
 import sys
 from settings import *
+from level import Level
 
 class Game:
     def __init__(self):
         
-        #general setup
+        # general setup
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        pygame.display.set_caption('Game Titel')
         self.clock = pygame.time.Clock()
+
+        self.level = Level()
 
     def run(self):
         while True:
@@ -18,6 +22,7 @@ class Game:
                     sys.exit()
 
             self.screen.fill('black')
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
